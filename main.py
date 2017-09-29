63241c6b496499d7bb4e977af01409d494253869
Greg Darke <darke@google.com>
2017-09-29 15:51:18 +1000

    A somewhat working version.
    This version has the basic skeleton required to defer work to taskqueue,
    storing our state inside datastore.
    
    This code should handle multiple concurrent executions (a rare but
    possible thing that can happen with Task Queue).

--- main.py
+++ main.py
@@ -1,89 +1,144 @@
 #!/usr/bin/env python
 
-import webapp2
-import models
+import hashlib
 import json
+import logging
+import models
+import random
+import urllib
+import webapp2
 
 from google.appengine.api import taskqueue
+from google.appengine.ext import ndb
 from google.appengine.ext import deferred
 
 
 class HelloWorld(webapp2.RequestHandler):
   def get(self, **kwargs):
-    self.response.out.write('Sup?') 
+    if self.request.get('task_name', None) is None:
+      self.response.out.write('''Wat
+      <form action='/'  method='POST'>
+      <textarea name='json'></textarea>
+      <input type='submit'>
+      </form>
+      ''')
+      return
+    archive_key = models.Archive.Key(self.request.get('task_name'))
+    archive_entity = archive_key.get()
+    if archive_entity.gcs_url:
+      return webapp2.redirect(archive_entity.gcs_url.encode('utf-8'))
+    else:
+      self.response.out.write('Still Waiting') 
 
   def post(self):
     swarming_request = json.loads(self.request.get('json'))
-    archive_key = models.Archive.Key(swarming_request['base_tast_name'])
+    archive_key = models.Archive.Key(swarming_request['base_task_name'])
 
     @ndb.transactional
     def _():
-      archive_entity = archive_key.Get()
+      archive_entity = archive_key.get()
       if not archive_entity:
         archive_entity = models.Archive(
             key=archive_key,
             initial_request=swarming_request)
         archive_entity.put()
-        deferred.defer(PollArchive, key=archive_key, _transactional=True)
+
+        # Insert all of the tasks as entities under our parent.
+        for json_task_name, json_task in swarming_request['tasks'].iteritems():
+          # The id for this key is auto generated (thus why we specify 0).
+          task_key = ndb.Key(models.Task, 0, parent=archive_key)
+          task = models.Task(
+              key=task_key, 
+              task_id=json_task['task_id'],
+              task_name=json_task_name,
+              url='https://chromium-swarm.appspot.com/api/swarming/v1/task/%s/result' % json_task['task_id'])
+          task.put()
+        # Only poll the archive if this commit succeeds.
+        deferred.defer(
+            PollArchive, archive_key=archive_key,
+            _transactional=True,
+            _retry_options=taskqueue.TaskRetryOptions(
+              max_backoff_seconds=6.0,
+              min_backoff_seconds=3.0,
+              task_age_limit=60*60,  # 1 hour
+              ))
       return archive_entity
     archive_entity = _()
+    return webapp2.redirect(
+        '/?%s' % urllib.urlencode({'task_name': swarming_request['base_task_name']}))
 
 
-  archive_entity = archive_key.Get()
-  not_finished = []
-  for task in archive_entity.tasks:
-    task.hash = poll(task.url)
+def DoUploadToGCS(archive_entity):
+  tasks = list(models.Task.TasksQuery(parent_key=archive_entity.key))
+  if random.random() < 0.1:
+    assert False, 'Oops, an error occurred'
+  return 'http://localhost.localdomain/%s' % archive_entity.key.string_id()
 
-    @ndb.transactional
-    def check_all_done():
-      archive_entity_update = archive_key.Get()
+def PollSingleTask(url):
+  r = random.random()
+  if r < 0.5:
+    return hashlib.sha256(url).hexdigest()
+  elif 0.5 <= r < 0.6:
+    assert False, 'Oops, an error occurred'
+  else:
+    return None
 
-      # Don't poll tasks already completed
-      if archive_entity_update['task'].hash is not None:
-        continue
-
-      if not task.hash:
-        not_finished.append(task.url)
-
-      # Save the updated task state. If this fails, we'll just run again.
-      archive_entity_update.put()
-
-    return archive_entity if all_finished else None
-  archive_entity = check_all_done()
-
-
-
-def PollArchive(key):
+def PollArchive(archive_key):
   @ndb.transactional
-  def check_all_done():
-    archive_entity = archive_key.Get()
+  def get_tasks_to_process():
+    ret = []
+    for task in models.Task.TasksQuery(parent_key=archive_key):
+      if task.hash is None:
+        # Still work to do
+        ret.append(task)
+    return ret
+  tasks_to_process = get_tasks_to_process()
 
-    all_finished = True
-    for task in archive_entity.tasks:
-      # Don't poll tasks already completed
-      if task.completed:
-        continue
-
-      task.hash = poll(task.url)
-      if not task.hash:
-        all_finished = False
-
-    # Save the updated task state. If this fails, we'll just run again.
+  @ndb.transactional
+  def update_task_with_hash(task_key, new_hash):
+    task = task_key.get()
+    if task.hash is None: 
+      task.hash = new_hash
+      task.put()
+    elif task.hash != new_hash:
+      logging.error('Found a conflicting hash for %r, was %r is now %r', task_key, task.hash, new_hash)
+    
+  all_finished = True
+  for task in tasks_to_process:
+    new_hash = PollSingleTask(task.url)
+    if not new_hash:
+      all_finished = False
+      continue
+    update_task_with_hash(task.key, new_hash)
+  
+  if not all_finished:
+    assert False, 'Need to defer the task into the future to try again.'
+    
+  @ndb.transactional
+  def _():
+    archive_entity = archive_key.get()
+    if archive_entity.polling_finished:
+      return
+    archive_entity.polling_finished = True
     archive_entity.put()
-    return archive_entity if all_finished else None
-  archive_entity = check_all_done()
-  if archive_entity is not None:
-    assert False, 'Not finished'
+    deferred.defer(
+        UploadToGCS, archive_key=archive_key,
+        _transactional=True,
+        _retry_options=taskqueue.TaskRetryOptions(
+          max_backoff_seconds=60.0, min_backoff_seconds=30.0))
+  _()
 
+def UploadToGCS(archive_key):
+  archive_entity = archive_key.get()
   if archive_entity.gcs_url:
     # We are already done, nothing to do here
     return
-
-  uploaded_location = upload_to_gcs(archive_entity)
+  # NOTE: It is possible that this function may be called twice simultaneously (so we need to handle that case)
+  uploaded_location = DoUploadToGCS(archive_entity)
 
   @ndb.transactional
   def update_gcs_url(uploaded_location):
-    archive_entity = archive_key.Get()
+    archive_entity = archive_key.get()
     if archive_entity.gcs_url and archive_entity.gcs_url != uploaded_location:
       # Someone else already uploaded the blob. Delete our copy.
       return False
@@ -93,7 +148,6 @@ def PollArchive(key):
   if not update_gcs_url(uploaded_location):
     # Fixme: Delete our blob, it is a duplicate.
     pass
-
 
 Application = webapp2.WSGIApplication(
   routes=[
--- models.py
+++ models.py
@@ -1,13 +1,21 @@
+import hashlib
+
 from google.appengine.ext import ndb
 
 class Task(ndb.Model):
+  task_id = ndb.TextProperty()
+  task_name = ndb.TextProperty()
   url = ndb.TextProperty()
   hash = ndb.BlobProperty()
+  
+  @classmethod
+  def TasksQuery(cls, parent_key):
+    return cls.query(ancestor=parent_key)
 
 
 class Archive(ndb.Model):
-  tasks = ndb.LocalStructuredProperty(Isolates, repeated=True)
   gcs_url = ndb.TextProperty()
+  polling_finished = ndb.BooleanProperty()
   created_date = ndb.DateTimeProperty(auto_now_add=True)
   last_updated_date = ndb.DateTimeProperty(auto_now=True)
 
